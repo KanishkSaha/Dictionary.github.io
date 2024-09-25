eda18dc61a1d68d5392f67a33860ee2a6efca44a
KanishkSaha <kanishksaha63@gmail.com>
2024-09-25 09:23:22 +0530

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -6,10 +6,34 @@ let allMusic = [
     src: "Sweetest Surprise"
   },
   {
+    name: "Show Me The Meaning",
+    artist: "BLUE",
+    img: "Show Me The Meaning",
+    src: "Show Me The Meaning"
+  },
+  {
     name: "Family Tree",
     artist: "MLTR",
     img: "Family Tree",
     src: "Family Tree"
+  },
+  {
+    name: "Uptown Girl",
+    artist: "Westlife",
+    img: "Uptown Girl",
+    src: "Uptown Girl"
+  },
+  {
+    name: "There's Us",
+    artist: "Backstreet Boys",
+    img: "There's Us",
+    src: "There's Us"
+  },
+  {
+    name: "Everybody (Backstreet's Back)",
+    artist: "Backstreet Boys",
+    img: "Everybody (Backstreet's Back)",
+    src: "Everybody (Backstreet's Back)"
   },
   {
     name: "You Took My Heart",
@@ -18,10 +42,22 @@ let allMusic = [
     src: "You Took My Heart"
   },
   {
+    name: "One Love",
+    artist: "BLUE",
+    img: "One Love",
+    src: "One Love"
+  },
+  {
     name: "Sleeping Child",
     artist: "MLTR",
     img: "Sleeping Child",
     src: "Sleeping Child"
+  },
+  {
+    name: "Elements",
+    artist: "BLUE",
+    img: "Elements",
+    src: "Elements"
   },
   {
     name: "I'm Gonna Be Around",
@@ -30,10 +66,22 @@ let allMusic = [
     src: "I'm Gonna Be Around"
   },
   {
+    name: "You've Lost",
+    artist: "Westlife",
+    img: "You've Lost",
+    src: "You've Lost"
+  },
+  {
     name: "Home To You",
     artist: "MLTR",
     img: "Home To You",
     src: "Home To You"
+  },
+  {
+    name: "I do",
+    artist: "Westlife",
+    img: "I do",
+    src: "I do"
   },
   {
     name: "That's Why (You Go Away)",
@@ -42,9 +90,33 @@ let allMusic = [
     src: "That's Why (You Go Away)"
   },
   {
+    name: "Nothings Gonna Change",
+    artist: "Westlife",
+    img: "Nothings Gonna Change",
+    src: "Nothings Gonna Change"
+  },
+  {
     name: "Blue Night",
     artist: "MLTR",
     img: "Blue Night",
     src: "Blue Night"
+  },
+  {
+    name: "The Reason Why",
+    artist: "Westlife",
+    img: "The Reason Why",
+    src: "The Reason Why"
+  },
+  {
+    name: "As Long As You Love Me",
+    artist: "Backstreet Boys",
+    img: "As Long As You Love Me",
+    src: "As Long As You Love Me"
+  },
+  {
+    name: "I'm Gonna Miss You",
+    artist: "Westlife",
+    img: "I'm Gonna Miss You",
+    src: "I'm Gonna Miss You"
   },
 ];
